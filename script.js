055b36ba2b6117fabaf2430ae190eda9063d2bb1
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-23 20:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+
+
+// Do not change the code above this
+// add your promises to the array `promises`
 window.promises = [];
+//your JS code here. If required.
+promises=["i Love u","I hate u","I belive you","sold","pick"]
+const divElement=document.getElementById("output")
+
+for(let x in promises){
+    document.writeln(divElement.innerText=promises[x])
+}
 
 // Do not change the code above this
 // add your promises to the array `promises`
