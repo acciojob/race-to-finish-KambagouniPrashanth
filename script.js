35b507f206ae222eff79eba0c80b44fd42a78985
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-30 16:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 
 
-// Do not change the code above this
-// add your promises to the array `promises`
-window.promises = [];
-//your JS code here. If required.
-promises.push("Ravi");
-promises.push("kam");
-promises.push("vari");
-promises.push("story");
-promises.push("proper");
-const divElement=document.getElementById("output")
+// // Do not change the code above this
+// // add your promises to the array `promises`
+// window.promises = [];
+// //your JS code here. If required.
+// promises=["first","second","third","fourth","fifth"]
+// const divElement=document.getElementById("output")
+// let time=1000;
+// for(let x=0;x<promises.length;x++){
+//     //console.log(promises[x])
+//     setTimeout(()=>{divElement.innerText=promises[x]},time)
+//     //console.log(time)
+//     time=time+1000;
+        
 
-for(let x in promises){
-    document.writeln(divElement.innerText=promises[x])
-}
+    
+// }
+
+setTimeout(()=>{
+    divElement.innerText="this first promise"
+    },1000)
+    setTimeout(()=>{
+        divElement.innerText="this second promise"
+        },2000)
+        setTimeout(()=>{
+            divElement.innerText="this first promise"
+            },3000)
+            setTimeout(()=>{
+                divElement.innerText="this first promise"
+                },4000)
+                setTimeout(()=>{
+                    divElement.innerText="this first promise"
+                    },5000)
+
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
