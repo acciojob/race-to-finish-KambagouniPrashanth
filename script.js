923278f538c4d357ac9cc0ebc7d988c436bc9f0d
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-07-01 17:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
+function makeNetworkCall(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const responsedata=[
+                "chnnai",
+                "bengolore",
+                "mumbai",
+                "hyderabad",
+                "pune"
+            ]
+            resolve(responsedata)
+        },3000)
+    })
+}
+const container=document.getElementById("output")
+
+function appendDataOntoUi(citilist){
+	
+    
+    citilist.forEach((city)=>{
+        const p=document.createElement("p");
+         p.innerText=city;
+         container.appendChild(p)
 
 
-// // Do not change the code above this
-// // add your promises to the array `promises`
-// window.promises = [];
-// //your JS code here. If required.
-// promises=["first","second","third","fourth","fifth"]
-// const divElement=document.getElementById("output")
-// let time=1000;
-// for(let x=0;x<promises.length;x++){
-//     //console.log(promises[x])
-//     setTimeout(()=>{divElement.innerText=promises[x]},time)
-//     //console.log(time)
-//     time=time+1000;
-        
+    })
 
     
-// }
-const divElement=document.getElementById("output")
 
+}
 
-setTimeout(()=>{
-    divElement.innerText="this first promise"
-    },1000)
-    setTimeout(()=>{
-        divElement.innerText="this second promise"
-        },2000)
-        setTimeout(()=>{
-            divElement.innerText="this first promise"
-            },3000)
-            setTimeout(()=>{
-                divElement.innerText="this first promise"
-                },4000)
-                setTimeout(()=>{
-                    divElement.innerText="this first promise"
-                    },5000)
+let promise=makeNetworkCall();
 
+promise.then((data)=>{
+    appendDataOntoUi(data)
 
+})
 
-// Do not change the code above this
-// add your promises to the array `promises`
