2199a43e0b000b5ab0f9ce63da5c22f273919233
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-07-06 11:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-function makeNetworkCall(){
-    return new Promise((resolve,reject)=>{
+let a=new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            const responsedata=[
-                "chnnai",
-                "bengolore",
-                "mumbai",
-                "hyderabad",
-                "pune"
-            ]
-            resolve(responsedata)
+            resolve("a")
         },3000)
     })
-}
-const container=document.getElementById("output")
-
-function appendDataOntoUi(citilist){
-	
     
-    citilist.forEach((city)=>{
-        const p=document.createElement("p");
-         p.innerText=city;
-         container.appendChild(p)
-
-
+    let b=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            reject("b")
+        },4000)
     })
-
     
-
-}
-
-let promise=makeNetworkCall();
-
-promise.then((data)=>{
-    appendDataOntoUi(data)
-
-})
-
+    let c=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("c")
+        },5000)
+    })
+let d=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("d")
+        },1000)
+    })
+let e=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("e")
+        },2000)
+    })
+    let x=Promise.any([a,b,c,d,e])
+    // setTimeout(()=>{
+    //     console.log(x)
+    
+    // },4000)
+    x.then((data)=>{
+        const divElement=document.getElementById("output")
+       divElement.innerText=data;
+    })
